af315577fe4c62a3911bd70f9bf2e43e05f97dd9
Anurag Bhatt <anuragbhatt1805@gmail.com>
2025-06-03 00:36:02 +0530

    fix: format Terraform remote state configuration for consistency

--- terraform/main/service/main.tf
+++ terraform/main/service/main.tf
@@ -3,9 +3,9 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "core" {
   backend = "s3"
   config = {
-    bucket       = var.remote_state_bucket
-    key          = var.key
-    region       = var.region
+    bucket = var.remote_state_bucket
+    key    = var.key
+    region = var.region
   }
 }
 
